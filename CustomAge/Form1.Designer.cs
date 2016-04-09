aa50d5aeb0e437527f6993368aeb404638e9f004
Maria <mbochkarev@yahoo.com>
2016-04-08 22:10:58 -0700

    'message'

---
+++ CustomAge/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CustomAge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.age_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Validate Age";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // age_txt
+            // 
+            this.age_txt.Location = new System.Drawing.Point(83, 65);
+            this.age_txt.Name = "age_txt";
+            this.age_txt.Size = new System.Drawing.Size(100, 20);
+            this.age_txt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.age_txt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox age_txt;
+    }
+}
+
---
+++ CustomAge/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomAge
+{
+    public partial class Form1 : Form
+    {
+        private int age = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            age = int.Parse(age_txt.Text);
+            try
+            {
+                if (age <= 0)
+                {
+                    throw new AgeException();
+                }
+                MessageBox.Show("Your age is " + age.ToString());
+
+            }
+            catch (AgeException nje)
+            {
+                MessageBox.Show(nje.Message);
+
+            }
+        }
+    }
+    public class AgeException : Exception
+    {
+        public AgeException()
+            :base(" Ooop !! Age must be greater than 0! Please re-enter !"){
+        }
+    }
+}       
+        
